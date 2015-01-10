56dd275f11c74d5262200635d0b343931f11f46f
alojzije <shljunak@hotmail.com>
2015-01-10 17:08:18 +0100

    Add some styling and make data into a JSON obj

--- js/checkIJE.js
+++ js/checkIJE.js
@@ -1,52 +1,98 @@
-var ijeDB = (function() {
-
-    // {'correct spelling of the word' :'optional HTML explanation'}
-    var data = {
-        'izmijeniti': false,
-        'obavijestiti': '<b> Pojma nemam je li ovo točno </b>'
-    };
-
-    var checkIJE = function(inputWord) {
-        var regEx = new RegExp('^' + inputWord.replace(/ije|je/i, '[i]{0,1}je') + '$');
-        for (var word in data) {
-            if (word.match(regEx)) {
-                return {
-                    'correctSpelling': word,
-                    'explanation': data[word]
-                };
-            }
-        }
-        return {
-            'correctSpelling': 'ne znam :(',
-            'explanation': false
-        };
-    };
-
-    return {
-        checkIJE: checkIJE,
-    };
-})();
-
 $(document).ready(function() {
+
+    var IJEchecker = (function() {
+        // {'correct spelling of the word' :'optional HTML explanation'}
+        var data = null;
+
+        $.getJSON("js/IJEdata.json", function(json) {
+            data = json;
+            console.log(data)
+        });
+
+        var checkIJE = function(inputWord) {
+            var response = {
+                'correctSpelling': 'ne znam :(',
+                'explanation': false
+            };
+
+            if (data[inputWord]) {
+                response.correctSpelling = inputWord;
+                response.explanation = data[inputWord];
+            } else {
+                var pattern = inputWord.replace(/ije|je/i, '[i]{0,1}je')
+                    .replace(/c|ć|č/i, '[cčć]{1}')
+                    .replace(/s/i, '[sš]{1}')
+                    .replace(/z/i, '[zž]{1}');
+                var regEx = new RegExp('^' + pattern + '$');
+                for (var word in data) {
+                    if (word.match(regEx)) {
+                        response.correctSpelling = word;
+                        response.explanation = data[word];
+                        break;
+                    }
+                }
+            }
+            return response;
+        };
+
+        return {
+            checkIJE: checkIJE,
+        };
+    })();
+
+
+    var resultModule = (function() {
+        var resultContainer = $('.resultContainer');
+        var resultOutput = $('#resultOutput');
+        var explanationContainer = $('.explanationContainer');
+        var explanationOutput = $('#explanationOutput');
+        var IJEchecker = null;
+        resultContainer.fadeOut();
+
+        var setIJEchecker = function(IJEcheckerObj) {
+            IJEchecker = IJEcheckerObj;
+        };
+
+        var displayOutputForWord = function(word) {
+            var responseObj = IJEchecker.checkIJE(word.toLowerCase());
+            console.log(responseObj);
+            resultOutput.text(responseObj.correctSpelling);
+            resultContainer.fadeIn(200);
+
+            if (responseObj.explanation) {
+                explanationOutput.html(responseObj.explanation);
+                explanationContainer.fadeIn(200);
+            } else {
+                explanationContainer.fadeOut(200);
+                explanationOutput.html('');
+            }
+        };
+
+        var hideResultContainer = function() {
+            resultContainer.fadeOut(200);
+            explanationContainer.fadeOut(200);
+            resultOutput.text('');
+            explanationOutput.html('');
+        };
+
+        return {
+            setIJEchecker: setIJEchecker,
+            displayOutputForWord: displayOutputForWord,
+            hideResultContainer: hideResultContainer
+        };
+
+    })();
+
     var inputField = $('#inputWord');
-    var explanation = $('#optionalExplanation');
-    var result = $('#result');
+    resultModule.setIJEchecker(IJEchecker);
 
     $('#ijeForm').submit(function(e) {
         e.preventDefault();
         var inputWord = inputField.val();
         if (inputWord) {
-            var res = ijeDB.checkIJE(inputWord.toLowerCase());
-            result.text(res.correctSpelling);
-            if (res.explanation) {
-                explanation.html(res.explanation);
-            } else {
-                explanation.html('');
-            }
-        }
-        else{
-        	result.text('');
-        	explanation.html('');
+            resultModule.displayOutputForWord(inputWord);
+        } else {
+            resultModule.hideResultContainer();
         }
     });
 
